43130f031b8950bd089a8b0591b0bb6e0c258227
priagopal <31636593+priagopal@users.noreply.github.com>
2022-08-04 09:02:29 -0400

    Update GiBleed.sql

--- inst/sql/sql_server/GiBleed.sql
+++ inst/sql/sql_server/GiBleed.sql
@@ -9,7 +9,8 @@ SELECT CAST(@cohort_definition_id AS INT) AS cohort_definition_id,
 	condition_start_date,
 	condition_end_date
 FROM @cdm_database_schema.condition_occurrence
-WHERE condition_concept_id IN (
+WHERE condition_end_date IS NOT NULL and
+      condition_concept_id IN (
 		SELECT descendant_concept_id
 		FROM @cdm_database_schema.concept_ancestor
 		WHERE ancestor_concept_id = 192671 -- Gastrointestinal haemorrhage
