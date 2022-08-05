a4117d99032202fee38029b79d7f3206c1b6396b
priagopal <31636593+priagopal@users.noreply.github.com>
2022-08-05 14:40:30 -0400

    Update GiBleed.sql (#24)
    Fix GiBleed.sql

--- inst/sql/sql_server/GiBleed.sql
+++ inst/sql/sql_server/GiBleed.sql
@@ -7,7 +7,7 @@ INSERT INTO @cohort_database_schema.@cohort_table (
 SELECT CAST(@cohort_definition_id AS INT) AS cohort_definition_id,
 	condition_occurrence.person_id,
 	condition_start_date,
-	condition_end_date
+	COALESCE(condition_end_date, condition_start_date)
 FROM @cdm_database_schema.condition_occurrence
 WHERE condition_concept_id IN (
 		SELECT descendant_concept_id
