b8e8e017e4be93af169f9e3108343d9f429519b3
Adam Black <ablack3@users.noreply.github.com>
2023-03-03 18:32:09 -0500

    Fix #22

--- inst/sql/sql_server/GiBleed.sql
+++ inst/sql/sql_server/GiBleed.sql
@@ -5,9 +5,9 @@ INSERT INTO @cohort_database_schema.@cohort_table (
 	cohort_end_date
 	)
 SELECT CAST(@cohort_definition_id AS INT) AS cohort_definition_id,
-	condition_occurrence.person_id,
-	condition_start_date,
-	condition_end_date
+	condition_occurrence.person_id AS subject_id,
+	condition_start_date AS cohort_start_date,
+	COALESCE(condition_end_date, DATEADD(day,1,condition_start_date)) AS cohort_end_date
 FROM @cdm_database_schema.condition_occurrence
 WHERE condition_concept_id IN (
 		SELECT descendant_concept_id
